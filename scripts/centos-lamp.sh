969bc38d6e6376bd3e4399fd22343906ebc7e28b
Dennis Yip <t80532292@hotmail.com>
2015-10-25 14:41:23 -0700

    Corrected info.php RAW link

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -33,7 +33,7 @@ mysql -u root -e "SHOW DATABASES";
 # Download Starter Content
 cd /vagrant
 sudo -u vagrant wget -q https://raw.githubusercontent.com/t80532292/vagrant/master/files/index.html
-sudo -u vagrant wget -q https://github.com/t80532292/vagrant/blob/master/files/info.php
+sudo -u vagrant wget -q https://raw.githubusercontent.com/t80532292/vagrant/master/files/info.php
 
 
 service httpd start
