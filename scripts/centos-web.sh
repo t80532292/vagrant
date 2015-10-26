e82fe56d2ec729ce9a438999580aa200ee0dab2d
Dennis Yip <t80532292@hotmail.com>
2015-10-25 19:55:31 -0700

    added wget files from github

--- scripts/centos-web.sh
+++ scripts/centos-web.sh
@@ -1,5 +1,11 @@
 #!/bin/env bash
 
+# Update Centos with any patches
+yum update -y --exclude=kernel 
+
+# Tools
+yum install -y nano git unzip screen
+
 # Apache
 yum install -y httpd httpd-devel httpd-tools
 chkconfig --add httpd
@@ -14,4 +20,13 @@ service httpd start
 
 # PHP
 yum install -y php php-cli php-common php-devel php-mysql
+
+
+# Download Starter Content
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/t80532292/vagrant/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/t80532292/vagrant/master/files/info.php
+
+
+service httpd start
 
